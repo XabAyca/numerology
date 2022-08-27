2ecb9429220713d16024de36b0994b0de450c241
Xabi <xabi.aycaguer@gmail.com>
2022-08-27 19:35:14 +0200

    Add: devise

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem "haml-rails"
 
 gem 'simple_form'
 
+gem "devise"
+
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3", ">= 7.0.3.1"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
 
   # Defines the root path route ("/")
   root "static_pages#index"
+  devise_for :users
 end
