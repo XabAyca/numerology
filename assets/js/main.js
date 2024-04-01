bd06ef02e1f170c593c73a8846701c4f390623b7
Xabi <xabi.aycaguer@gmail.com>
2024-04-01 17:26:05 +0200

    :constructor: wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,150 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const bubblyButtons = document.getElementsByClassName("bubbly-button");
+  const submitBtn = document.getElementById("submit_btn");
+  const userFormular = document.getElementById("user_formular");
+  const reTestBtn = document.getElementById("retest");
+  const blockFormular = document.getElementById("formular");
+  const blockresponse = document.getElementById("response");
+  const firstNameInput = document.getElementById("floatingFirstName");
+  const lastNameInput = document.getElementById("floatingLastName");
+  const dateInput = document.getElementById("floatingDate");
+  let letterValue;
+  async function getLetterValue() {
+    const reponse = await fetch("./data/letter_value.json");
+    letterValue = await reponse.json();
+  }
+  getLetterValue();
 
+  // Btn Animation //
   const animateButton = (e) => {
-    e.preventDefault;
-
     e.target.classList.remove("animate");
-
     e.target.classList.add("animate");
     setTimeout(function () {
       e.target.classList.remove("animate");
     }, 700);
   };
 
+  submitBtn.addEventListener("click", animateButton);
 
-  for (let i = 0; i < bubblyButtons.length; i++) {
-    bubblyButtons[i].addEventListener("click", animateButton, false);
-  }
-})
+  // Toggl block //
+  const togglblock = (e) => {
+    let isValid = userFormular.checkValidity();
 
+    if (!isValid) {
+      userFormular.reportValidity();
+    } else {
+      e.preventDefault();
+      setTimeout(function () {
+        if (e.target == submitBtn) {
+          displayResponse();
+          blockFormular.style.display = "none";
+          blockresponse.style.display = "block";
+        } else if (e.target == reTestBtn) {
+          resetFields();
+          blockFormular.style.display = "block";
+          blockresponse.style.display = "none";
+        }
+      }, 700);
+    }
+  };
+
+  const resetFields = () => {
+    inputs = blockFormular.getElementsByTagName("input");
+    for (let i = 0, max = inputs.length; i < max; i++) {
+      inputs[i].value = null;
+    }
+  };
+
+  submitBtn.addEventListener("click", togglblock);
+  reTestBtn.addEventListener("click", togglblock);
+
+  // Calcul helpers //
+  const baseString = (string) => {
+    return string
+      .toLowerCase()
+      .match(/\p{Ll}/gu)
+      .join("");
+  };
+
+  const onlyVowels = (string) => {
+    return baseString(string)
+      .match(/[aeiouàáâãäåæèéêëìíîïòóôõöùúûüýÿ]/g)
+      .join("");
+  };
+
+  const onlyConsonants = (string) => {
+    return baseString(string)
+      .match(/[^aeiouàáâãäåæèéêëìíîïòóôõöùúûüýÿ]/g)
+      .join("");
+  };
+
+  const calculBaseNine = (number) => {
+    let result = number
+      .toString()
+      .split("")
+      .map((digit) => parseInt(digit))
+      .reduce((a, b) => {
+        return a + b;
+      });
+
+    if (result > 9) {
+      return calculBaseNine(result);
+    } else {
+      return result;
+    }
+  };
+
+  const month = () => {
+    return dateInput.value.split("-")[1];
+  };
+
+  const day = () => {
+    return dateInput.value.split("-")[2];
+  };
+
+  const year = () => {
+    return dateInput.value.split("-")[0];
+  };
+
+  // Calculs //
+  const lifeRoad = () => {
+    return calculBaseNine(parseInt(`${day()}${month()}${year()}`));
+  };
+
+  const numberOfAchievements = () => {
+    return calculBaseNine(parseInt(`${day()}${month()}`));
+  };
+
+  const personnalYear = () => {
+    // Année universelle + jourN + MoisN
+    return calculBaseNine(parseInt(`${day()}${month()}`));
+  };
+
+  const birthDayVibration = () => {
+    // ?????????
+    return calculBaseNine(parseInt(`${day()}`));
+  };
+
+  const spiritualInitiation = () => {
+    // Expression + ch de vie + elan spirit + jour N
+    return calculBaseNine(parseInt(`${day()}`));
+  };
+
+  // Display methods //
+  const lifeRoadHtml = document.getElementById("life_road");
+  const numberOfAchievementsHtml = document.getElementById(
+    "number_of_achievements"
+  );
+  const personnalYearHtml = document.getElementById("personnal_year");
+  const birthDayVibrationHtml = document.getElementById("birth_day_vibration");
+  const spiritualInitiationHtml = document.getElementById(
+    "spiritual_initiation"
+  );
+
+  const displayResponse = () => {
+    lifeRoadHtml.innerHTML = lifeRoad();
+    numberOfAchievementsHtml.innerHTML = numberOfAchievements();
+    personnalYearHtml.innerHTML = personnalYear();
+    birthDayVibrationHtml.innerHTML = birthDayVibration();
+    spiritualInitiationHtml.innerHTML = spiritualInitiation();
+  };
+});
